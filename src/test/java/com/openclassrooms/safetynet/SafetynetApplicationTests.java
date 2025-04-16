9e369cd7c53dbf2ae933e28ee7290e5fd57b8cfa
Kevin-Balderrama <kevin.balderrama97@gmail.com>
2025-04-15 21:03:18 -0400

    corrected accidental package issue

--- src/test/java/com/openclassrooms/safetynet/SafetynetApplicationTests.java
+++ src/test/java/com/openclassrooms/safetynet/SafetynetApplicationTests.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.safetnet.safetynet;
+package com.openclassrooms.safetynet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
